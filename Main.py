37702a55b58fb2add9b3072d1b0612c0dea9e7e2
JoseDavidVindas <jose.chavarria34@ulatina.net>
2024-04-23 15:33:36 -0600

    Se sube el proyecto

---
+++ Main.py
@@ -0,0 +1,8 @@
+verdadero = True
+falso = False
+resultado_and = verdadero and falso
+resultado_or = verdadero or falso
+resultado_not = not verdadero
+print("Resultado de verdadero AND falso:", resultado_and)
+print("Resultado de verdadero OR falso:", resultado_or)
+print("Resultado de NOT verdadero:", resultado_not)
